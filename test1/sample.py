256bf79bfaa6ab62e2b3cc77cb11882bdcc750af
martinokafor <foremeka@gmail.com>
2021-05-06 01:46:02 +0200

    All again

--- test1/sample.py
+++ test1/sample.py
@@ -2,7 +2,8 @@ import os
 import yaml
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+base_env =os.environ.get("NOSE_EN", "sample.yaml")
 
-with open(os.path.join(BASE_DIR, "./sample.yaml")) as file:
+with open(os.path.join(BASE_DIR, base_env)) as file:
 	env = yaml.load(file, Loader=yaml.FullLoader)
 print(env)
